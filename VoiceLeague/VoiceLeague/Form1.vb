a903028d1f33d11c8ead588e943c334f1522cc2d
voipleague <support@voipleague.com>
2020-03-11 18:05:08 +0000

    Update Form1.vb

--- VoiceLeague/VoiceLeague/Form1.vb
+++ VoiceLeague/VoiceLeague/Form1.vb
@@ -1,3 +1,10 @@
-﻿Public Class Form1
+﻿Public Class OtionsForm
+    Private Sub OtionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DefaultLookAndFeel1.EnableBonusSkins = True
+        DefaultLookAndFeel1.LookAndFeel.SetSkinStyle("UseDefault;Skin: Darkroom")
+    End Sub
 
+    Private Sub OtionsForm_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        DefaultLookAndFeel1.LookAndFeel.ActiveLookAndFeel.SetSkinStyle("Darkroom")
+    End Sub
 End Class
